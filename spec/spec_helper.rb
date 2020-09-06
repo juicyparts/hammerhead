155cf23a76cf4219c996829db6c7caca8dc02a7a
Mel Riffe <mel@juicyparts.com>
2020-09-05 22:43:22 -0400

    Update rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require "bundler/setup"
 require "hammerhead"
 
+require "ap"
+require "pry-byebug"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -8,7 +11,15 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  # Enable support for fit, fdescribe and fcontext
+  # or :focus metadata
+  config.filter_run_when_matching :focus
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+def fixtures_path
+  File.join(File.dirname(__FILE__), 'fixtures')
 end
