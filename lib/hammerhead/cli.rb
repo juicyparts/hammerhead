2074f8123ad26a5103ebdd7245c43f7f0e6fd61a
Mel Riffe <mel@juicyparts.com>
2020-09-06 02:35:39 -0400

    Create 'clients' Command
    This is the command to get a list of active clients from Harvest.
    Specifying --all returns all clients, except for those in the to be
    excluded list. Quick details are in the README.
    
    I disabled the Unit test because I haven't found a way to test an API
    call that doesn't require 'real' credentials.

--- lib/hammerhead/cli.rb
+++ lib/hammerhead/cli.rb
@@ -14,8 +14,29 @@ module Hammerhead
     desc 'version', 'hammerhead version'
     def version
       require_relative 'version'
-      puts "v#{Hammerhead::VERSION}"
+      puts "v#{Hammerhead::VERSION} Hammerhead 🦈"
     end
     map %w(--version -v) => :version
+
+    desc 'clients [OPTIONS]', 'Fetch list of clients from Harvest'
+    long_desc <<-DESC
+      Fetch list of clients from Harvest.
+
+      The default behavior is to return 'active' clients, only.
+
+      Add the --all flag to return entire list of clients.
+    DESC
+    method_option :all, aliases: '-a',
+      type: :boolean, desc: 'Return all clients from Harvest'
+    method_option :help, aliases: '-h',
+      type: :boolean, desc: 'Display usage information'
+    def clients(*)
+      if options[:help]
+        invoke :help, ['clients']
+      else
+        require_relative 'commands/clients'
+        Hammerhead::Commands::Clients.new(options).execute
+      end
+    end
   end
 end
---
+++ lib/hammerhead/commands/clients.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'tty-table'
+require "hammerhead"
+require_relative '../command'
+
+module Hammerhead
+  module Commands
+    class Clients < Hammerhead::Command
+      attr_reader :options
+      def initialize(options)
+        @options = options
+      end
+
+      def execute(input: $stdin, output: $stdout)
+
+        harvest = Hammerhead::HarvestClient.new
+        harvest.authenticate!
+        clients = harvest.clients options
+
+        output.puts "Pass the Id to the 'status_report' command, or enough of the name to uniquely match."
+        output.puts
+
+        table = TTY::Table.new headers, data( clients )
+        output.puts table.render(:unicode)
+
+        output.puts
+        output.puts "CLIENTS FOUND: #{clients.size}"
+
+      rescue => e
+        output.puts "CLIENTS COMMAND ERROR: #{e}"
+      end
+
+      private
+
+      def headers
+        ['Id', 'Name']
+      end
+
+      def data clients
+        clients.collect do |client|
+          [ client.id, client.name ]
+        end
+      end
+    end
+  end
+end
