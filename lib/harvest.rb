c016e7df9fd8968a27ba581575f38c27d0da47bd
Mel Riffe <mel@juicyparts.com>
2020-09-19 17:50:59 -0400

    Add rudimentary documentation to Harvest module

--- lib/harvest.rb
+++ lib/harvest.rb
@@ -2,8 +2,14 @@
 
 require 'harvest/connection'
 
+##
+# Exposes the Harvest API to Hammerhead.
+#
 module Harvest
   class << self
+    ##
+    # Returns the sole Harvest::Connection instance.
+    #
     def connection
       Harvest::Connection.instance
     end
--- lib/harvest/connection.rb
+++ lib/harvest/connection.rb
@@ -4,6 +4,12 @@ require 'singleton'
 require 'harvested' # NOTE: support for Harvest API V1
 
 module Harvest
+  ##
+  # :markup: markdown
+  # Represents the link between Harvest and Hammerhead and the Harvest V1 API.
+  #
+  # Backed by ['harvested'](https://rubygems.org/gems/harvested) gem.
+  #
   class Connection
     include Singleton
     include Hammerhead::Utils
@@ -12,38 +18,72 @@ module Harvest
     # Use .instance to grab an initialied harvest connection:
     #   connection = Harvest::Connection.instance
     #
+    # Because this attempts to establish a connection to the API, ensure a
+    # correctly defined 'hammerhead.yml' file exists.
+    #
+    # Returns Harvest.hardy_client
+    #
     def initialize
       new_connection!
     end
 
+    ##
+    # This is the hammerhead user, as defined in by the harvest credentials. It
+    # is for this user the client list will be provided, and the status report
+    # will be generated.
+    #
     def authenticated_user
-      # harvest.users.all.first
       harvest.account.who_am_i
     end
 
-    def week_start_day
-      authenticated_user.company.week_start_day
+    ##
+    # For the specified +client_id+ return its Harvest definition.
+    #
+    # If +client_id+ contains alpha-characters NotImplementedError is raised.
+    #
+    def client client_id
+      raise NotImplementedError, 'Client by name is not implemented yet.' unless digits?(client_id)
+      harvest.clients.find client_id
     end
 
+    ##
+    # Return a list of clients. Currently only +options[:all]+ is supported.
+    # The default behavior is to only return 'active' clients.
+    #
+    # If the configuration file defines a list of ids to exclude, they're removed
+    # before 'active' or all clients are returned.
+    #
     def clients options = {}
       clients = harvest.clients.all.reject { |client| clients_to_exclude.include? client.id }
       clients = clients.select { |client| client.active == true } unless options['all']
       clients
     end
 
-    def client client_id
-      raise NotImplementedError, 'Client by name is not implemented yet.' unless digits?(client_id)
-      harvest.clients.find client_id
+    ##
+    # Return a list of timesheet entries between +start_date+ and +end_date+, inclusive.
+    #
+    # This is for the +authenticated_user+.
+    #
+    def my_time_sheet_entries start_date, end_date
+      harvest.reports.time_by_user authenticated_user, start_date, end_date
     end
 
+    ##
+    # Returns 'active' projects for the specified +client+.
+    #
+    # If the +client+ is not active, an empty list is returned.
+    #
     def projects_for_client client
       return [] unless client.active?
 
       harvest.reports.projects_by_client(client).select(&:active?)
     end
 
-    def my_time_sheet_entries start_date, end_date
-      harvest.reports.time_by_user authenticated_user, start_date, end_date
+    ##
+    # Return begining of work week as defined in Harvest
+    #
+    def week_start_day
+      authenticated_user.company.week_start_day
     end
 
     private
